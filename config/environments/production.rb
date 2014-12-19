e6e07f8d37ca8e5f08dcb648b8f710d511a7029c
Kari Burns <kariannechristina@gmail.com>
2014-12-19 13:35:22 -0800

    actionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'stark-island-6247.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery = :smtp
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
@@ -74,7 +74,9 @@ Rails.application.configure do
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
-  :enable_starttls_auto => true
+  :enable_starttls_auto => true,
+  :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+  :tls => true,
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

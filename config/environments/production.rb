8ff9aeeb9357740e4d8603f5940be1c076eb29db
Kari Burns <kariannechristina@gmail.com>
2014-12-18 17:08:38 -0800

    action-mailer default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.default_url_options = { :host => 'stark-island-6247.herokuapp.com' }
   # config.action_mailer.raise_delivery_errors = false
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
